4470f402c694e10219a227ea96b249edf5ea5c1c
Finn Martens <Finn.Martens@iteratec.de>
2019-06-25 16:39:21 +0200

    [IT-2742] Added scaffold for the aggregation chart and initialized OpenSpeedMonitor namespace for use of existing d3 component.

--- frontend/src/app/modules/aggregation/aggregation.component.ts
+++ frontend/src/app/modules/aggregation/aggregation.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ResultSelectionStore} from "../result-selection/services/result-selection.store";
 import {URL} from "../../enums/url.enum";
 import {BarchartDataService} from "../chart/services/barchart-data.service";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'osm-aggregation',
@@ -12,6 +13,9 @@ export class AggregationComponent implements OnInit {
 
   constructor(private barchartDataService: BarchartDataService, private resultSelectionStore: ResultSelectionStore) { }
 
+  barchartAverageData$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  barchartMedianData$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+
   ngOnInit() {
   }
 
@@ -21,14 +25,14 @@ export class AggregationComponent implements OnInit {
       this.resultSelectionStore.remainingGetBarchartCommand,
       "avg",
       URL.AGGREGATION_BARCHART_DATA
-    ).subscribe(result => console.log(result));
+    ).subscribe(result => this.barchartAverageData$.next(result));
 
     this.barchartDataService.fetchBarchartData<any>(
       this.resultSelectionStore.resultSelectionCommand,
       this.resultSelectionStore.remainingGetBarchartCommand,
       50,
       URL.AGGREGATION_BARCHART_DATA
-    ).subscribe(result => console.log(result));
+    ).subscribe(result => this.barchartMedianData$.next(result));
   }
 
   isGetBarchartDataAllowed(): boolean {
